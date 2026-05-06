810b3f1c8ed7231feb5c2160952a91e3881eda54
Oscar Rojas <rx3card@gmail.com>
2026-05-06 06:53:34 -0500

    Se definicio warp-terminal como la terminal predeterminada, con atajos de comandos adicionales.

--- modules/wm/layouts/layouts.c
+++ modules/wm/layouts/layouts.c
@@ -4,7 +4,7 @@ static const Rule rules[] = {
    *	WM_NAME(STRING) = title
    */
   /* class            instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-  { "Alacritty",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+  { "warp-terminal",      NULL,     NULL,           0,         0,          1,           0,        -1 },
   { "st-256color",    NULL,     NULL,           0,         0,          1,           0,        -1 },
   { MY_BROWSER,        NULL,     NULL,           0,         0,          0,          -1,        -1 },
   { NULL,             NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
